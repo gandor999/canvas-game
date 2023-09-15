9dbc84ec736201a812ef4d387f84821e6b429003
Geodor Ruales <geodorruales@gmail.com>
2023-09-16 04:41:50 +0800

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import World from './World.js'
+
+function createHiPPICanvas(width, height) {
+  const ratio = window.devicePixelRatio
+  const canvas = document.getElementById('canvas')
+
+  canvas.width = width * ratio
+  canvas.height = height * ratio
+  canvas.style.width = width + 'px'
+  canvas.style.height = height + 'px'
+  canvas.getContext('2d').scale(ratio, ratio)
+
+  return canvas
+}
+
+function setupCanvas() {
+  const canvas = createHiPPICanvas(500, 300)
+
+  const world = new World(canvas)
+
+  world.loadWorld()
+}
+
+window.addEventListener('load', () => {
+  setupCanvas()
+})
