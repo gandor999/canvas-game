92f5297cfa6976f72906fe16da75e09c1a1676ff
Geodor Ruales <geodorruales@gmail.com>
2023-09-16 22:02:05 +0800

    refactor: make functions to call animate and spawn methods of game objects

--- World.js
+++ World.js
@@ -1,10 +1,27 @@
 import Border from './Border.js'
-import Ball from './Ball.js'
+import Ball from './game-objects/Ball.js'
+import listenForMouse from './util/listenForMouse.js'
 
 export default class World {
   constructor(canvas) {
     this.canvas = canvas
     this.ctx = this.canvas.getContext('2d')
+  }
+
+  playWorld(gameObjects) {
+    requestAnimationFrame(() => {
+      this.playWorld(gameObjects)
+    }, canvas)
+    this.ctx.clearRect(0, 0, this.ctx.canvas.clientWidth, this.ctx.canvas.clientHeight)
+    gameObjects.forEach(gameObject => {
+      gameObject.animate()
+    })
+  }
+
+  spawnObjects(gameObjects) {
+    gameObjects.forEach(gameObject => {
+      gameObject.spawn()
+    })
   }
 
   loadWorld() {
@@ -13,7 +30,6 @@ export default class World {
 
     const ball = new Ball({
       canvas: this.canvas,
-
       innerColor: '#44CCFF',
       outerThickness: 1,
       pos: { x: 10, y: 10 },
@@ -21,26 +37,14 @@ export default class World {
       initVelocity: { xV: 10, yV: 15 },
     })
 
-    const ball2 = new Ball({
-      canvas: this.canvas,
+    const gameObjects = [ball]
 
-      innerColor: '#E0FF4F',
-      outerThickness: 1,
-      pos: { x: 50, y: 100 },
-      radius: 10,
-      initVelocity: { xV: 20, yV: 19 },
+    this.spawnObjects(gameObjects)
+
+    this.playWorld(gameObjects)
+
+    gameObjects.forEach(gameObject => {
+      listenForMouse(this.canvas, gameObject)
     })
-
-    ball.spawn()
-    ball2.spawn()
-
-    const playWorld = () => {
-      requestAnimationFrame(playWorld, canvas)
-      this.ctx.clearRect(0, 0, this.ctx.canvas.clientWidth, this.ctx.canvas.clientHeight)
-      ball.animate()
-      ball2.animate()
-    }
-
-    playWorld()
   }
 }
