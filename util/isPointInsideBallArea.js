5dd660ce14ec9cefb2b154febe8b7988cca45960
Geodor Ruales <geodorruales@gmail.com>
2023-09-16 22:04:58 +0800

    chore: remove debugging for isPointInsideBall function

--- util/isPointInsideBallArea.js
+++ util/isPointInsideBallArea.js
@@ -2,7 +2,6 @@ export default function isPointInsideBallArea(
   { mouseX, mouseY },
   { x: circleX, y: circleY, radius }
 ) {
-  console.log({ mouseY, circleY, yMinusR: circleY - 2 * radius })
   return (
     mouseX > circleX ||
     mouseX < circleX + 2 * radius ||
